c5ef47baaab7f7d7c1bc00a3111f2e15a74ca43b
Augusto Cezar <augusto.c.ferreira@hotmail.com>
2021-04-26 18:52:23 -0300

    arrumando api

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.get('/', async function(req, res, next) {
 router.post('/', async function(req,res,next){
 
   let users = await Data.insertUser(req.body.name, req.body.email, req.body.password);
-  res.status(200).json([message,{"Mensagem":"Usuário inserido com sucesso"},token,{"token":"123465"}]);
+  res.status(200).json({"data":{"mensagem" :"ususario inserido com sucesso"},"token":"4567813"});
   
 
 });
