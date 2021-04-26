0366c52bb5c1b2da5e418f9c50a8ac59eaf74e0f
Augusto Cezar <augusto.c.ferreira@hotmail.com>
2021-04-26 18:03:12 -0300

    first commit

---
+++ model/db.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = "mongodb+srv://admin:admin@cluster0.sflja.mongodb.net/backend?retryWrites=true&w=majority";
+
+
+module.exports = class Posts{
+
+    static async insertUser(name,email,password){
+        const conn = await MongoClient.connect(url);
+        const db = conn.db();
+        let result = await db.collection('users').insertOne({
+
+            name : name, 
+            email :email,
+            password : password
+        });
+
+        conn.close();
+
+        return result;
+    }
+
+    static async select(){
+        const conn = await MongoClient.connect(url);
+        const db = conn.db();
+       
+        let result = await db.collection('users').find().toArray();
+        conn.close();
+        return result;
+
+    }
+}
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+var Data = require('../model/db');
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+
+  let users = await Data.select();
+  res.json(users);
+  
+});
+
+router.post('/', async function(req,res,next){
+
+  let users = await Data.insertUser(req.body.name, req.body.email, req.body.password);
+  res.status(200).json({"Mensagem":"Usuário inserido com sucesso"});
+  
+
+});
+
+module.exports = router;
