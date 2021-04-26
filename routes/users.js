edf70f4479d490e0cfe8e147a7af335f54cad3b9
Augusto Cezar <augusto.c.ferreira@hotmail.com>
2021-04-26 20:31:56 -0300

    incluindo campo token

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.get('/', async function(req, res, next) {
 router.post('/', async function(req,res,next){
 
   let users = await Data.insertUser(req.body.name, req.body.email, req.body.password);
-  res.status(200).json({"data":{"mensagem" :"ususario inserido com sucesso"},"token":"4567813"});
+  res.status(200).json({"data":{"mensagem" :"ususario inserido com sucesso"},"token":""});
   
 
 });
