7841580a7204c78d3088c86c82e96e53ed83a34f
Augusto Cezar <augusto.c.ferreira@hotmail.com>
2021-04-28 12:04:09 -0300

    atualizando rota users

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var Data = require('../model/db');
 var express = require('express');
+const { findUser } = require('../model/db');
 var router = express.Router();
 
 
@@ -12,19 +13,29 @@ router.get('/', async function(req, res, next) {
 });
 
 router.post('/', async function(req,res,next){
+  let find = await Data.findUser(req.body.email, req.body.password);  
+  if(find){
+    res.status(404).json({"data":{"mensagem": "usuario já cadastrado"}});
 
-  try{
+  }else{
+      let users = await Data.insertUser(req.body.name, req.body.email, req.body.password);
+      res.status(200).json({"data":{"mensagem" :"ususario inserido com sucesso"},"token":""});
+
+  }
+
+  /*try{
     let find = await Data.findUser(req.body.email, req.body.password);
-    if (find.email == req.body.email)
+    if (find.email == req.body.email){
       res.status(404).json({"data":{"mensagem": "usuario já cadastrado"}});
+    }
 
   }catch{    
     
     
       let users = await Data.insertUser(req.body.name, req.body.email, req.body.password);
-      res.status(200).body.json({"data":{"mensagem" :"ususario inserido com sucesso"},"token":""});
+      res.status(200).json({"data":{"mensagem" :"ususario inserido com sucesso"},"token":""});
   
-  }
+  }*/
 
   
   
