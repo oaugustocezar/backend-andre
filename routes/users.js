f91d1fbe67e8f3381c330256f06b33a2112a163c
Augusto Cezar <augusto.c.ferreira@hotmail.com>
2021-04-28 11:45:53 -0300

    atualizando userssr

--- routes/users.js
+++ routes/users.js
@@ -20,8 +20,9 @@ router.post('/', async function(req,res,next){
 
   }catch{    
     
+    
       let users = await Data.insertUser(req.body.name, req.body.email, req.body.password);
-      res.status(200).json.body({"data":{"mensagem" :"ususario inserido com sucesso"},"token":""});
+      res.status(200).body.json({"data":{"mensagem" :"ususario inserido com sucesso"},"token":""});
   
   }
 
